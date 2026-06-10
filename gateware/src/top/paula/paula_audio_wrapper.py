9e11178bbbbae378742aa1fd2c83850c87074f1d
Mat Kelcey <matthew.kelcey@gmail.com>
2026-06-10 13:06:46 +1000

    wip paula_audio wrapper

---
+++ gateware/src/top/paula/paula_audio_wrapper.py
@@ -0,0 +1,71 @@
+import os
+
+from amaranth import *
+
+
+class PaulaAudioWrapper(Elaboratable):
+    """Amaranth wrapper for Minimig `paula_audio` and its dependent RTL files."""
+
+    def __init__(self):
+        self.clk7_en = Signal()
+        self.cck = Signal()
+        self.rst = Signal()
+        self.strhor = Signal()
+
+        self.reg_address_in = Signal(unsigned(8))
+        self.data_in = Signal(unsigned(16))
+        self.dmaena = Signal(unsigned(4))
+        self.audpen = Signal(unsigned(4))
+
+        self.audint = Signal(unsigned(4))
+        self.dmal = Signal(unsigned(4))
+        self.dmas = Signal(unsigned(4))
+        self.ldata = Signal(unsigned(15))
+        self.rdata = Signal(unsigned(15))
+        self.ldata_okk = Signal(unsigned(9))
+        self.rdata_okk = Signal(unsigned(9))
+
+    def add_verilog_sources(self, platform):
+        if platform is None or not hasattr(platform, "add_file"):
+            return
+
+        vroot = os.path.join(
+            os.path.dirname(os.path.realpath(__file__)),
+            "../../../deps/Minimig-AGA_MiSTer/rtl",
+        )
+
+        for file_name in [
+            "paula_audio.v",
+            "paula_audio_channel.v",
+            "paula_audio_mixer.v",
+            "paula_audio_volume.v",
+        ]:
+            with open(os.path.join(vroot, file_name), "r") as f:
+                platform.add_file(file_name, f.read())
+
+    def elaborate(self, platform):
+        m = Module()
+
+        self.add_verilog_sources(platform)
+
+        m.submodules.paula_audio = Instance(
+            "paula_audio",
+            i_clk=ClockSignal("sync"),
+            i_clk7_en=self.clk7_en,
+            i_cck=self.cck,
+            i_rst=self.rst,
+            i_strhor=self.strhor,
+            i_reg_address_in=self.reg_address_in,
+            i_data_in=self.data_in,
+            i_dmaena=self.dmaena,
+            o_audint=self.audint,
+            i_audpen=self.audpen,
+            o_dmal=self.dmal,
+            o_dmas=self.dmas,
+            o_ldata=self.ldata,
+            o_rdata=self.rdata,
+            o_ldata_okk=self.ldata_okk,
+            o_rdata_okk=self.rdata_okk,
+        )
+
+        return m
--- gateware/src/top/paula/paula_instance.py
+++ gateware/src/top/paula/paula_instance.py
@@ -5,6 +5,7 @@ from amaranth.lib.wiring import In, Out
 from tiliqua.dsp import ASQ
 
 from fake_agnus import FakeAgnus
+from paula_audio_wrapper import PaulaAudioWrapper
 from sample import Sample
 
 
@@ -26,9 +27,19 @@ class PaulaInstance(wiring.Component):
     def elaborate(self, platform):
         m = Module()
 
+        AUD0LEN = 0x52
+        AUD0PER = 0x53
+        AUD0VOL = 0x54
+        AUD0DAT = 0x55
+        AUD1LEN = 0x5A
+        AUD1PER = 0x5B
+        AUD1VOL = 0x5C
+        AUD1DAT = 0x5D
+
         m.submodules.sample0 = sample0 = Sample()
         m.submodules.sample1 = sample1 = Sample()
         m.submodules.fake_agnus = fake_agnus = FakeAgnus()
+        m.submodules.paudio = paudio = PaulaAudioWrapper()
 
         # Initial Paula-style register bank for channel playback control.
         aud_loc0 = Signal(unsigned(16), init=0)
@@ -41,40 +52,170 @@ class PaulaInstance(wiring.Component):
         aud_per1 = Signal(unsigned(16), init=0)
         aud_dmaen1 = Signal(init=0)
 
+        aud_dmaen0_prev = Signal(init=0)
+        aud_dmaen1_prev = Signal(init=0)
+        dma_toggle_evt0 = Signal()
+        dma_toggle_evt1 = Signal()
+        dma_start_evt0 = Signal()
+        dma_start_evt1 = Signal()
+
+        # Simple register write sequencer to set AUDx regs in order.
+        cfg_pending0 = Signal(unsigned(2), init=0)
+        cfg_pending1 = Signal(unsigned(2), init=0)
+        play_target0 = Signal(init=0)
+        play_target1 = Signal(init=0)
+        reg_addr = Signal(unsigned(8), init=0)
+        reg_data = Signal(unsigned(16), init=0)
+        dat_phase = Signal(init=0)
+
+        sample_shift = max(ASQ.as_shape().width - 8, 0)
+        out_shift = max(ASQ.as_shape().width - 15, 0)
+        sample0_q8 = Signal(signed(8))
+        sample1_q8 = Signal(signed(8))
+        sample0_word = Signal(unsigned(16))
+        sample1_word = Signal(unsigned(16))
+        pa_l = Signal(signed(15))
+        pa_r = Signal(signed(15))
+
         m.d.comb += [
+            dma_toggle_evt0.eq(aud_dmaen0 ^ aud_dmaen0_prev),
+            dma_toggle_evt1.eq(aud_dmaen1 ^ aud_dmaen1_prev),
+            dma_start_evt0.eq(aud_dmaen0 & ~aud_dmaen0_prev),
+            dma_start_evt1.eq(aud_dmaen1 & ~aud_dmaen1_prev),
+            sample0_q8.eq(sample0.o_sample.as_value() >> sample_shift),
+            sample1_q8.eq(sample1.o_sample.as_value() >> sample_shift),
+            sample0_word.eq(Cat(sample0_q8.as_unsigned(), sample0_q8.as_unsigned())),
+            sample1_word.eq(Cat(sample1_q8.as_unsigned(), sample1_q8.as_unsigned())),
+            pa_l.eq(paudio.ldata.as_signed()),
+            pa_r.eq(paudio.rdata.as_signed()),
             sample0.i_sample.eq(self.i_sample0),
             sample0.sample_tick.eq(self.sample_tick),
             sample0.record_toggle_evt.eq(self.i_record_evt0),
-            sample0.playback_evt.eq(self.i_play_evt0),
+            # Sample playback follows Paula voice DMA state changes.
+            sample0.playback_evt.eq(dma_toggle_evt0),
             sample1.i_sample.eq(self.i_sample1),
             sample1.sample_tick.eq(self.sample_tick),
             sample1.record_toggle_evt.eq(self.i_record_evt1),
-            sample1.playback_evt.eq(self.i_play_evt1),
-            self.o_sample0.eq(sample0.o_sample),
-            self.o_sample1.eq(sample1.o_sample),
-            # Placeholder DMA handshake wiring for the future feeder contract.
-            fake_agnus.i_audio_dmal.eq(0),
-            fake_agnus.i_audio_dmas.eq(0),
-            fake_agnus.i_reg_write.eq(0),
-            fake_agnus.i_reg_addr.eq(0),
-            fake_agnus.i_reg_data.eq(0),
+            sample1.playback_evt.eq(dma_toggle_evt1),
+            # Route Paula voices through L/R: v0->left(out0), v1->right(out1).
+            self.o_sample0.as_value().eq(pa_l << out_shift),
+            self.o_sample1.as_value().eq(pa_r << out_shift),
+            # Fake Agnus observes Paula's DMA/restart requests for future handoff.
+            fake_agnus.i_audio_dmal.eq(Cat(paudio.dmal[0], paudio.dmal[1])),
+            fake_agnus.i_audio_dmas.eq(Cat(paudio.dmas[0], paudio.dmas[1])),
+            fake_agnus.i_reg_write.eq(reg_addr != 0),
+            fake_agnus.i_reg_addr.eq(reg_addr),
+            fake_agnus.i_reg_data.eq(reg_data),
+            paudio.clk7_en.eq(self.sample_tick),
+            paudio.cck.eq(self.sample_tick),
+            paudio.rst.eq(0),
+            paudio.strhor.eq(self.sample_tick),
+            paudio.reg_address_in.eq(reg_addr),
+            paudio.data_in.eq(reg_data),
+            # First two Paula voices map to the two sample pads.
+            paudio.dmaena.eq(Cat(aud_dmaen0, aud_dmaen1, C(0, 2))),
+            paudio.audpen.eq(0),
         ]
 
         # Playback MIDI events now also update Paula-like registers.
         with m.If(self.i_play_evt0):
-            m.d.sync += [
-                aud_loc0.eq(0),
-                aud_len0.eq(Sample.MAX_CAPTURE_SAMPLES),
-                aud_per0.eq(Sample.CAPTURE_DENOM),
-                aud_dmaen0.eq(~aud_dmaen0),
-            ]
+            with m.If(aud_dmaen0):
+                m.d.sync += [
+                    play_target0.eq(0),
+                    aud_dmaen0.eq(0),
+                    cfg_pending0.eq(0),
+                ]
+            with m.Else():
+                m.d.sync += [
+                    aud_loc0.eq(0),
+                    aud_len0.eq(Sample.MAX_CAPTURE_SAMPLES),
+                    aud_per0.eq(Sample.CAPTURE_DENOM),
+                    play_target0.eq(1),
+                    cfg_pending0.eq(1),
+                ]
 
         with m.If(self.i_play_evt1):
-            m.d.sync += [
-                aud_loc1.eq(0),
-                aud_len1.eq(Sample.MAX_CAPTURE_SAMPLES),
-                aud_per1.eq(Sample.CAPTURE_DENOM),
-                aud_dmaen1.eq(~aud_dmaen1),
-            ]
+            with m.If(aud_dmaen1):
+                m.d.sync += [
+                    play_target1.eq(0),
+                    aud_dmaen1.eq(0),
+                    cfg_pending1.eq(0),
+                ]
+            with m.Else():
+                m.d.sync += [
+                    aud_loc1.eq(0),
+                    aud_len1.eq(Sample.MAX_CAPTURE_SAMPLES),
+                    aud_per1.eq(Sample.CAPTURE_DENOM),
+                    play_target1.eq(1),
+                    cfg_pending1.eq(1),
+                ]
+
+        # Channel register/data feed into paula_audio.
+        m.d.sync += [
+            reg_addr.eq(0),
+            reg_data.eq(0),
+        ]
+
+        with m.If(self.sample_tick):
+            m.d.sync += dat_phase.eq(~dat_phase)
+            # Feed paula_audio AUDxDAT continuously from current sample outputs.
+            with m.If(dat_phase == 0):
+                m.d.sync += [
+                    reg_addr.eq(AUD0DAT),
+                    reg_data.eq(sample0_word),
+                ]
+            with m.Else():
+                m.d.sync += [
+                    reg_addr.eq(AUD1DAT),
+                    reg_data.eq(sample1_word),
+                ]
+            # Background channel setup writes.
+            with m.If(cfg_pending0 != 0):
+                with m.Switch(cfg_pending0):
+                    with m.Case(1):
+                        m.d.sync += [
+                            reg_addr.eq(AUD0LEN),
+                            reg_data.eq(aud_len0),
+                            cfg_pending0.eq(2),
+                        ]
+                    with m.Case(2):
+                        m.d.sync += [
+                            reg_addr.eq(AUD0PER),
+                            reg_data.eq(aud_per0),
+                            cfg_pending0.eq(3),
+                        ]
+                    with m.Case(3):
+                        m.d.sync += [
+                            reg_addr.eq(AUD0VOL),
+                            reg_data.eq(64),
+                            cfg_pending0.eq(0),
+                            aud_dmaen0.eq(play_target0),
+                        ]
+            with m.Elif(cfg_pending1 != 0):
+                with m.Switch(cfg_pending1):
+                    with m.Case(1):
+                        m.d.sync += [
+                            reg_addr.eq(AUD1LEN),
+                            reg_data.eq(aud_len1),
+                            cfg_pending1.eq(2),
+                        ]
+                    with m.Case(2):
+                        m.d.sync += [
+                            reg_addr.eq(AUD1PER),
+                            reg_data.eq(aud_per1),
+                            cfg_pending1.eq(3),
+                        ]
+                    with m.Case(3):
+                        m.d.sync += [
+                            reg_addr.eq(AUD1VOL),
+                            reg_data.eq(64),
+                            cfg_pending1.eq(0),
+                            aud_dmaen1.eq(play_target1),
+                        ]
+
+        m.d.sync += [
+            aud_dmaen0_prev.eq(aud_dmaen0),
+            aud_dmaen1_prev.eq(aud_dmaen1),
+        ]
 
         return m
